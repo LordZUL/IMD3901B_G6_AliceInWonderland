0f2a662b59b02e9aff88353fd5456c42d8002f12
Setaish Qayomi <setaish1qayomi@gmail.com>
2026-02-12 19:04:48 -0500

    UI

--- Assets/Scripts/CrosshairUI.cs
+++ Assets/Scripts/CrosshairUI.cs
@@ -1,7 +1,6 @@
-// Temporary: will be renamed to itemInteraction instead!!
-
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CrosshairUI : MonoBehaviour
 {
@@ -9,8 +8,15 @@ public class CrosshairUI : MonoBehaviour
     public Color normalColor = Color.white;
     public Color interactColor = Color.pink;
 
+    public GameObject interactionText; 
+
     public void SetInteract(bool canInteract)
     {
         crosshairImage.color = canInteract ? interactColor : normalColor;
+
+        if (interactionText != null)
+        {
+            interactionText.SetActive(canInteract);
+        }
     }
 }
