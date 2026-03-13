bfd51f8885622a52b6bb6ead2b27eb26205bab78
Zhe <alannayou.z@gmail.com>
2026-03-13 06:52:15 -0400

    Make things "sticky"
    kinematic

--- Assets/Scripts/Objects/Sticky.cs
+++ Assets/Scripts/Objects/Sticky.cs
@@ -2,27 +2,31 @@ using UnityEngine;
 
 public class Sticky : MonoBehaviour
 {
+    private Rigidbody rb;
     private Renderer objectRenderer;
     void Start()
     {
         objectRenderer = GetComponent<Renderer>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void OnTriggerEnter(Collider other)
     {
         // Check if the object that entered is interactable
-        if (other.CompareTag("Interactable"))
+        if (other.CompareTag("NotInteractable"))
         {
-            objectRenderer.material.color = Color.green;
+            //objectRenderer.material.color = Color.green;
+            rb.isKinematic = true;
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Interactable"))
+        if (other.CompareTag("NotInteractable"))
         {
-            objectRenderer.material.color = Color.red;
+            //objectRenderer.material.color = Color.red;
+            rb.isKinematic = false;
         }
     }
 }
