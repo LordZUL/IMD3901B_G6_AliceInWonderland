afca7eff3cad258f33bc242208920822fecb489e
marissecharette <124939657+marissecharette@users.noreply.github.com>
2026-03-28 22:57:15 -0400

    made changes to cat animation loop script

--- Assets/Scripts/Room2/catAnimationLoop.cs
+++ Assets/Scripts/Room2/catAnimationLoop.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class catAnimationLoop : MonoBehaviour
 {
     public Animator animator;
+    public Renderer cat;
     // Loop time, accounts for the animation time which is 12s. So the cat animation loops 3s after the animation is done.
-    public float loop = 15f;
+    public float loop = 30f;
+    public float animationLength = 13f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,8 +22,16 @@ public class catAnimationLoop : MonoBehaviour
     {
         while (true)
         {
+            // Make cat visible when animation plays
+            cat.enabled = true;
+
             // Start animation
             animator.Play("path", 0 , 0f);
+            yield return new WaitForSeconds(animationLength);
+
+            // Hide cat after animation plays
+            cat.enabled = false;
+
             yield return new WaitForSeconds(loop);
         }
     }
