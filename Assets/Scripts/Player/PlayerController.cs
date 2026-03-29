02204bbe7d75edaa9d4f1878d8211c26439871f2
Zhe <alannayou.z@gmail.com>
2026-03-29 08:42:11 -0400

    Changed Player controller movement and rotation
    According to Player control playlist: https://youtube.com/playlist?list=PLBcfp6HMOJwzDcdCzoAx3jJKm7sIcBXJZ&si=snXGOItQbdXOjrj0

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,6 +2,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 // Added Player Jumping: https://www.youtube.com/watch?v=cKPdSKBM4rs -> Player control
+//Player control playlist: https://youtube.com/playlist?list=PLBcfp6HMOJwzDcdCzoAx3jJKm7sIcBXJZ&si=snXGOItQbdXOjrj0
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
@@ -9,10 +10,26 @@ public class PlayerController : MonoBehaviour
     public bool canMove = true;
     private Vector2 _input;
     private CharacterController _characterController;
+
+    // in the youtube tutorial, he used direction of keyboard to determine the direction where camera is facing. To make it so it follows mouse movement I made following changes
     private Vector3 _direction;
 
+    [SerializeField] private float mouseSensitivity = 1f;
+    [SerializeField] private Transform cameraTransform;
+    private float xRotation = 0f;
+
+
+    [SerializeField] private float smoothTime = 0.05f;
+    private float _currentVelocity;
     [SerializeField] private float speed;
 
+    // Jumping variables:
+    private float _gravity = -9.81f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
@@ -20,12 +37,42 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        _characterController.Move(_direction * speed *Time.deltaTime);
+        /*
+        //return angle in radius * Rad2Deg to get degree
+        if (_input.sqrMagnitude == 0) return;
+
+        var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+        //smooth out turn
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+        _characterController.Move(_direction * speed *Time.deltaTime);*/
+
+        if (!canMove) return;
+
+        // ===== MOUSE LOOK =====
+        Vector2 mouseDelta = Mouse.current.delta.ReadValue();
+
+        float mouseX = mouseDelta.x * mouseSensitivity;
+        float mouseY = mouseDelta.y * mouseSensitivity;
+
+        // vertical camera rotation (up/down)
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // horizontal player rotation (left/right)
+        transform.Rotate(Vector3.up * mouseX);
+
+        // ===== MOVEMENT =====
+        Vector3 move = transform.right * _direction.x + transform.forward * _direction.z;
+
+        _characterController.Move(move * speed * Time.deltaTime);
     }
     public void Move(InputAction.CallbackContext context)
     {
         _input = context.ReadValue<Vector2>();
-        _direction = new Vector3(_input.x, y: 0.0f, z: _input.y);
+        _direction = new Vector3(_input.x, 0.0f, _input.y);
     }
 
 
