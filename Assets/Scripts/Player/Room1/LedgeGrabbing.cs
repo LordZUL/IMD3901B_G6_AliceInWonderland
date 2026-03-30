1b29653af77b20a6362238d9fa60cdd9f58778d0
Zhe <alannayou.z@gmail.com>
2026-03-30 14:21:28 -0400

    fix scale real quick
    max the scale fixed sized instead of multiply a certain number

--- Assets/Scripts/Player/Room1/LedgeGrabbing.cs
+++ Assets/Scripts/Player/Room1/LedgeGrabbing.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class LedgeGrabbing : MonoBehaviour
 {
+    private Rigidbody rb;
+    bool hanging;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        LedgeGrab();
+    }
+
+    void LedgeGrab()
+    {
+        // is player falling and not hanging
+        if (rb.linearVelocity.y < 0 && !hanging)
+        {
+            RaycastHit downHit;
+            Vector3 lineDownStart;
+            Vector3 lineDownEnd;
+        }
     }
 }
