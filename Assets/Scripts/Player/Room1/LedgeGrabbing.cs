4e6e6a89cb36f37f9f2235d7cc7ce39f8747a131
Zhe <alannayou.z@gmail.com>
2026-03-30 14:31:10 -0400

    Changing Layers for objects able to vault

--- Assets/Scripts/Player/Room1/LedgeGrabbing.cs
+++ Assets/Scripts/Player/Room1/LedgeGrabbing.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+//tutorial based on https://www.youtube.com/watch?v=EOn4IPEJf8k
 public class LedgeGrabbing : MonoBehaviour
 {
     private Rigidbody rb;
@@ -22,8 +22,9 @@ public class LedgeGrabbing : MonoBehaviour
         if (rb.linearVelocity.y < 0 && !hanging)
         {
             RaycastHit downHit;
-            Vector3 lineDownStart;
-            Vector3 lineDownEnd;
+            Vector3 lineDownStart = (transform.position + Vector3.up*3f) + transform.forward;
+            Vector3 lineDownEnd = (transform.position + Vector3.up * 1.5f) + transform.forward;
+            Physics.Linecast(lineDownStart, lineDownEnd, out downHit, )
         }
     }
 }
