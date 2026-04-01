5d19c5e0ed626a3dbb7efe9a9bcabe3bbf8ecf55
Zhe <alannayou.z@gmail.com>
2026-04-01 03:58:39 -0400

    Jumping working!!

--- Assets/Scripts/Room1/VR/VRGravity.cs
+++ Assets/Scripts/Room1/VR/VRGravity.cs
@@ -1,9 +1,52 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+
 [RequireComponent(typeof(CharacterController))]
 public class VRGravity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     private CharacterController controller;
+    private Vector3 velocity;
+
+    public float gravity = -9.81f;
+    public float gravityMultiplier = 2f;
+    public float jumpForce = 5f;
+
+    public InputActionReference primaryButton; // RIGHT primary
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void OnEnable()
+    {
+        primaryButton.action.Enable();
+    }
+
+    void Update()
+    {
+        // ===== GROUND CHECK =====
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // ===== JUMP =====
+        if (primaryButton.action.WasPressedThisFrame() && controller.isGrounded)
+        {
+            velocity.y = jumpForce;
+            Debug.Log("JUMP!");
+        }
+
+        // ===== GRAVITY =====
+        velocity.y += gravity * gravityMultiplier * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    /*private CharacterController controller;
     private Vector3 velocity;
 
     public float gravity = -9.81f;
@@ -24,5 +67,5 @@ public class VRGravity : MonoBehaviour
         velocity.y += gravity * gravityMultiplier * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
-    }
+    }*/
 }
