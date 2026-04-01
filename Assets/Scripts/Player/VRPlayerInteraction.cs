c7302f83a720e2a3b797f60b07f85388982a363d
marissecharette <124939657+marissecharette@users.noreply.github.com>
2026-03-31 22:04:38 -0400

    finished room 2 vr

---
+++ Assets/Scripts/Player/VRPlayerInteraction.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections;
+
+public class VRPlayerInteraction : MonoBehaviour
+{
+    // Door and scene switching
+    public UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable doorInteractable;
+    public ScreenFade screenFade;
+    public AudioClip nextScene;
+
+    // Rabbit hint
+    public UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable rabbitInteractable;
+    public DialogueNPC3D npcDialogue;
+
+    private AudioSource ac;
+
+    bool doorTriggered = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ac = GetComponent<AudioSource>();
+
+        doorInteractable.selectEntered.AddListener(OnDoorPoked);
+        rabbitInteractable.selectEntered.AddListener(OnRabbitPoked);
+    }
+
+    // Update is called once per frame
+    void OnDoorPoked(SelectEnterEventArgs args)
+    {
+        if (doorTriggered)
+        {
+            return;
+        }
+        
+        Debug.Log("Loading next room...");
+        doorTriggered = true;
+        StartCoroutine(LoadNextScene());
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        ac.PlayOneShot(nextScene);
+        
+        // Calls the FadeToBlack IEnumerator from the ScreenFade script 
+        yield return StartCoroutine(screenFade.FadeToBlack());
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void OnRabbitPoked(SelectEnterEventArgs args)
+    {
+        npcDialogue.TryStartDialogue();
+        Debug.Log("Rabbit poked");
+    }
+}
