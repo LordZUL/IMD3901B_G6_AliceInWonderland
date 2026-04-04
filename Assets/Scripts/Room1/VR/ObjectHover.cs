12bce065385a07ffdc1c74beb0cfe3545c4e8dfa
Zhe <alannayou.z@gmail.com>
2026-04-04 13:50:57 -0400

    white outline to carrots and mushroom -> have to apply to the others

--- Assets/Scripts/Room1/VR/ObjectHover.cs
+++ Assets/Scripts/Room1/VR/ObjectHover.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
-
+using UnityEngine.XR.Interaction.Toolkit;
 public class ObjectHover : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Renderer rend;
+    public Color hoverColor = Color.white;
+    private Color originalColor;
+
     void Start()
     {
-        
+        if (rend == null)
+            rend = GetComponent<Renderer>();
+
+        originalColor = rend.material.GetColor("_Color");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnHoverEnter()
     {
-        
+        //rend.material.SetColor("_Color", hoverColor);
+        var mats = rend.materials;
+        mats[1].SetColor("_Color", hoverColor);
+    }
+
+    public void OnHoverExit()
+    {
+        var mats = rend.materials;
+        mats[1].SetColor("_Color", originalColor);
+        //rend.material.SetColor("_Color", originalColor);
     }
 }
