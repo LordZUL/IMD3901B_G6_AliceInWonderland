e3e2f7133ffe88a32f43dd4803362c9f763d7b6c
Zhe <alannayou.z@gmail.com>
2026-04-04 14:14:25 -0400

    selected object now turn white too

--- Assets/Scripts/Room1/VR/ObjectHover.cs
+++ Assets/Scripts/Room1/VR/ObjectHover.cs
@@ -6,6 +6,7 @@ public class ObjectHover : MonoBehaviour
     public Renderer rend;
     public Color hoverColor = Color.white;
     private Color originalColor;
+    public bool isSelected = false;
 
     void Start()
     {
@@ -18,14 +19,36 @@ public class ObjectHover : MonoBehaviour
     public void OnHoverEnter()
     {
         //rend.material.SetColor("_Color", hoverColor);
-        var mats = rend.materials;
-        mats[1].SetColor("_Color", hoverColor);
+        if (!isSelected)
+        {
+            var mats = rend.materials;
+            mats[1].SetColor("_Color", hoverColor);
+        }
     }
 
     public void OnHoverExit()
     {
+        if (!isSelected)
+        {
+            var mats = rend.materials;
+            mats[1].SetColor("_Color", originalColor);
+        }
+        //rend.material.SetColor("_Color", originalColor);
+    }
+
+    public void OnSelectEnter()
+    {
+        isSelected = true;
+
+        var mats = rend.materials;
+        mats[1].SetColor("_Color", hoverColor);
+    }
+
+    public void OnSelectExit()
+    {
+        isSelected = false;
+
         var mats = rend.materials;
         mats[1].SetColor("_Color", originalColor);
-        //rend.material.SetColor("_Color", originalColor);
     }
 }
